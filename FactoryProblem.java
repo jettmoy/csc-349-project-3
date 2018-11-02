470e06314c2ec8166f3965379037bbe9c1698815
Andrew Exton <aexton@calpoly.edu>
2018-11-01 20:48:22 -0700

    more

--- FactoryProblem.java
+++ FactoryProblem.java
@@ -3,115 +3,112 @@ import java.io.*;
 import java.lang.*;
 
 public class FactoryProblem {
-   public static void main(String args[]) {
+      public static void main(String args[]) {
 
-      // Open input file
-      Scanner sc = new Scanner(System.in);
-      System.out.print("Input file: ");
+            // Open input file
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Input file: ");
 
-      File input = new File(sc.nextLine());
-      sc.close();
+            File input = new File(sc.nextLine());
+            sc.close();
 
-      try {
-         Scanner scanner = new Scanner(input);
+            try {
+                  Scanner scanner = new Scanner(input);
 
-      int n = scanner.nextInt();
-      System.out.println(n);
+                  int n = scanner.nextInt();
+                  System.out.println(n);
 
-      // entry time
-      int[] e = {scanner.nextInt(), scanner.nextInt()};
-      System.out.println(e[0] + " " + e[1]);
+                  // entry time
+                  int[] e = { scanner.nextInt(), scanner.nextInt() };
+                  System.out.println(e[0] + " " + e[1]);
 
-      // one station check
-      if (!scanner.hasNextInt()) {
-         //edge case
+                  // one station check
+                  if (!scanner.hasNextInt()) {
+                        // edge case
+                  }
+
+                  // exit time
+                  int[] x = { scanner.nextInt(), scanner.nextInt() };
+                  System.out.println(x[0] + " " + x[1]);
+
+                  // station time
+                  int[] a1 = new int[n], a2 = new int[n];
+                  for (int i = 0; i < n; i++)
+                        a1[i] = scanner.nextInt();
+                  for (int i = 0; i < n; i++)
+                        a2[i] = scanner.nextInt();
+                  for (int z : a1)
+                        System.out.print(z + " ");
+                  System.out.println();
+                  for (int z : a2)
+                        System.out.print(z + " ");
+                  System.out.println();
+
+                  // transfer time
+                  int[] t1 = new int[n - 1], t2 = new int[n - 1];
+                  for (int i = 0; i < n - 1; i++)
+                        t1[i] = scanner.nextInt();
+                  for (int i = 0; i < n - 1; i++)
+                        t2[i] = scanner.nextInt();
+                  for (int z : t1)
+                        System.out.print(z + " ");
+                  System.out.println();
+                  for (int z : t2)
+                        System.out.print(z + " ");
+                  System.out.println();
+
+                  scanner.close();
+
+                  fastestWay(a1, a2, t1, t2, e, x, n);
+
+            } catch (FileNotFoundException e) {
+                  System.out.println("FILE ERROR.");
+                  System.exit(1);
+            }
       }
 
-      // exit time
-      int[] x = {scanner.nextInt(), scanner.nextInt()};
-      System.out.println(x[0] + " " + x[1]);
+      public static void fastestWay(int[] a1, int[] a2, int[] t1, int[] t2, int[] e, int[] x, int n) {
+            int[] f1 = new int[n], f2 = new int[n];
+            int[] l1 = new int[n - 1], l2 = new int[n - 1];
+            int fC, lC;
+            f1[0] = e[0] + a1[0];
+            f2[0] = e[1] + a2[1];
+            for (int i = 1; i < n; i++) {
+                  if (f1[i - 1] + a1[i] <= f2[i - 1] + t2[i] + a1[i]) {
+                        // line 1 faster
+                        f1[i] = f1[i - 1] + a1[i];
+                        l1[i] = 1;
+                  } else {
+                        // line 2 faster
+                        f1[i] = f2[i - 1] + t2[i] + a1[i];
+                        l1[i] = 2;
+                  }
+                  if (f2[i - 1] + a2[i] <= f1[i - 1] + t1[i] + a1[i]) {
+                        // line 2 faster
+                        f2[i] = f2[i - 1] + a2[i];
+                        l2[i] = 2;
+                  } else {
+                        // line 1 faster
+                        f2[i] = f1[i - 1] + t1[i] + a2[i];
+                        l2[i] = 1;
+                  }
+            }
 
-      // station time
-      int[] a1 = new int[n], a2 = new int[n];
-      for (int i = 0; i < n; i++)
-         a1[i] = scanner.nextInt();
-      for (int i = 0; i < n; i++)
-         a2[i] = scanner.nextInt();
-      for (int z : a1)
-         System.out.print(z + " ");
-      System.out.println();
-      for (int z : a2)
-         System.out.print(z + " ");
-      System.out.println();
+            // calc exit times
+            if (f1[n] + x[0] <= f2[n] + x[1]) {
+                  fC = f1[n] + x[0];
+                  lC = 1;
+            } else {
+                  fC = f2[n] + x[1];
+                  lC = 2;
+            }
 
-      // transfer time
-      int[] t1 = new int[n-1], t2 = new int[n-1];
-      for (int i = 0; i < n - 1; i++)
-         t1[i] = scanner.nextInt();
-      for (int i = 0; i < n - 1; i++)
-         t2[i] = scanner.nextInt();
-      for (int z : t1)
-         System.out.print(z + " ");
-      System.out.println();
-      for (int z : t2)
-         System.out.print(z + " ");
-      System.out.println();
+            int line = lC;
+            System.out.println("line " + line + ", station " + n);
+            for (int i = n; i >= 2; i--) {
+                  line = line == 1 ? a1[i] : a2[i];
+                  System.out.println("line " + line + ", station " + (i - 1));
+            }
 
-      scanner.close();
-
-      fastestWay(a1, a2, t1, t2, e, x, n);
-
-   } catch (FileNotFoundException e) {
-         System.out.println("FILE ERROR.");
-         System.exit(1);
       }
-   }
-
-   public static void fastestWay(int[] a1, int[] a2, int[] t1, int[] t2, int[] e, int[] x, int n) {
-      int[] f1 = new int[n], f2 = new int[n];
-      int[] l1 = new int[n - 1], l2 = new int[n - 1];
-      int fC, lC;
-      f1[0] = e[0] + a1[0];
-      f2[0] = e[1] + a2[1];
-      for (int i = 1; i < n; i++) {
-         if (f1[i - 1] + a1[i] <= f2[i - 1] + t2[i] + a1[i]) {
-            // line 1 faster
-            f1[i] = f1[i - 1] + a1[i];
-            l1[i] = 1;
-         }
-         else {
-            // line 2 faster
-            f1[i] = f2[i - 1] + t2[i] + a1[i];
-            l1[i] = 2;
-         }
-         if (f2[i - 1] + a2[i] <= f1[i - 1] + t1[i] + a1[i]) {
-            //line 2 faster
-            f2[i] = f2[i - 1] + a2[i];
-            l2[i] = 2;
-         }
-         else {
-            // line 1 faster
-            f2[i] = f1[i - 1] + t1[i] + a2[i];
-            l2[i] = 1;
-         }
-      }
-
-      // calc exit times
-      if (f1[n] + x[0] <= f2[n] + x[1]) {
-         fC = f1[n] + x[0];
-         lC = 1;
-      }
-      else {
-         fC = f2[n] + x[1];
-         lC = 2;
-      }
-
-      int line = lC;
-      System.out.println("line " + line + ", station " + n);
-      for (int i = n; i >= 2; i--) {
-         line = line == 1 ? a1[i] : a2[i];
-         System.out.println("line " + line + ", station " + (i - 1));
-      }
-
-   }
 }
--- GameProblem.java
+++ GameProblem.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.io.*;
 import java.lang.*;
+import java.math.*;
 
 public class GameProblem {
     public static void main(String[] args) {
@@ -30,10 +31,12 @@ public class GameProblem {
             int n = scanner.nextInt();
             int m = scanner.nextInt();
 
-            int[][] A = new int[n][m];
+            // System.out.println("N: " + n + " M: " + m);
 
-            for (int i=0; i<n; i++) {
-                for (int j=0; j<m; j++) {
+            int[][] A = new int[n + 1][m + 1];
+
+            for (int i=1; i<=n; i++) {
+                for (int j=1; j<=m; j++) {
                     A[i][j] = scanner.nextInt();
                 }
             }
@@ -51,20 +54,58 @@ public class GameProblem {
     static int[][] S;
     static char[][] R;
     public static void game(int n, int m, int[][]A) {
-        S = new int[n][m];
-        R = new char[n][m];
+        S = new int[n + 1][m + 1];
+        R = new char[n + 1][m + 1];
+        find_optimal_solution(n, m, A);
+        print_optimal_score();
+        // print_optimal_route();
+
     }
 
-    // i, j: find_optimal_solution when starting at A[i,j]
-    private static void find_optimal_solution(int i, int j, int n, int m, int[][] A) {
-       
-        // if bottom right square (base case)
-        if (i >= n && j>= m) {
-            S[i][j] = A[i][j];
-        } else {
-            
+    private static void find_optimal_solution(int n, int m, int[][] A) {
+
+        S[n][m] = A[n][m];
+        
+        // build last column solutions
+        // last column -> move down OR exit
+        // max{𝑆[𝑖+1, 𝑚],0}+𝐴[𝑖,𝑚]
+        for (int i=m-1; i > 0; i--) {
+            S[i][m] = Math.max(S[i + 1][m], 0) + A[i][m];
         }
 
+        // build last row solutions
+        // last row -> move right OR exit
+        // max{𝑆[𝑛,𝑗+1],0}+𝐴[𝑛,𝑗]
+        for (int j=n-1; j>0; j--) {
+            S[n][j] = Math.max(S[n][j + 1], 0) + A[n][j];
+        }
+
+        print_2d_int_array(S);
+
+        for (int i=n-1; i>0; i--) {
+            for (int j=m-1; j>0; j--) {
+
+                // check whats better -> move down OR right
+                // max{𝑆[𝑖+1, 𝑗],𝑆[𝑖,𝑗+1]}+𝐴[𝑖,𝑗]
+                S[i][j] = Math.max(S[i + 1][j], S[i][j + 1]) + A[i][j];
+
+            }
+        }
+
+        print_2d_int_array(S);
+
+    }
+
+    private static void print_optimal_score() {
+        int optimalScore = Integer.MIN_VALUE;
+        
+        for (int i=0; i<S.length; i++) {
+            for (int j=0; j<S[0].length; j++) {
+                if (S[i][j] > optimalScore) optimalScore = S[i][j];
+            }
+        }
+
+        System.out.println("Best score: " + optimalScore);
     }
 
     /*
@@ -76,5 +117,14 @@ public class GameProblem {
      * down, “r” if the choice is to move right, and “e” if you have to exit.
      */
     private static void print_optimal_route() {
+    }
+
+    private static void print_2d_int_array(int[][] A) {
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
